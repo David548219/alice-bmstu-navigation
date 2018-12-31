df4bfe8cf4ff94e55652628932f5c384357ef9ae
SirDavidIII <david548219@gmail.com>
2018-12-31 04:08:16 +0300

    Updated Navigation module
    Changed links to pointers, updated to work with Storage

--- includes/Navigation.hpp
+++ includes/Navigation.hpp
@@ -6,61 +6,65 @@
 #include <algorithm>
 #include <functional>
 #include <limits>
+#include <queue>
 #include <stack>
 #include <string>
 #include <vector>
+#include "Storage.hpp"
 
 namespace nav {
-class Container {
- public:
-  std::string id;
-  int value;
-  Container() {
-    id = "nil";
-    value = -1;
-  }
-  Container(const std::string& _id, int _value) {
-    id = _id;
-    value = _value;
-  }
-  ~Container() {}
-};
 class DijkstraGraph {
  public:
+  class DijkstraNode;
+  class DijkstraBranch;
+
   class DijkstraNode {
    public:
     double nodeWeight;
     bool isVisited;
-    std::vector<std::pair<std::string, double>> neighbors;
-    Container container;
+    std::vector<DijkstraBranch*> branches;
+    nav::MapNode* container;
     DijkstraNode() {
       nodeWeight = std::numeric_limits<double>::infinity();
       isVisited = false;
-      container = Container();
+      container = new nav::MapNode();
     }
-    DijkstraNode(Container& _container) {
+    DijkstraNode(nav::MapNode* _container) {
       nodeWeight = std::numeric_limits<double>::infinity();
       isVisited = false;
       container = _container;
     }
     ~DijkstraNode() {}
   };
-  std::vector<DijkstraNode> nodes;
+  class DijkstraBranch {
+   public:
+    DijkstraNode* node;
+    double weight;
+    nav::BranchType branchType;
+    DijkstraBranch(DijkstraNode* _node,
+                   double _weight,
+                   nav::BranchType _branchType) {
+      node = _node;
+      weight = _weight;
+      branchType = _branchType;
+    }
+  };
+  std::vector<DijkstraNode*> nodes;
 
-  DijkstraNode& FindNode(const std::string& _id);
-  void AddNode(Container& _origin);
-  void AddBranch(const std::string& first,
-                 const std::string& second,
-                 double weight);
-  const std::vector<DijkstraNode>& GetNodes();
+  DijkstraNode* FindNode(const std::string& _id);
+  void AddNode(nav::MapNode* _container);
+  void AddBranch(const std::string& _firstId,
+                 nav::BranchType _firstBranchType,
+                 const std::string& _secondId,
+                 nav::BranchType _secondBranchType,
+                 double _weight);
+  const std::vector<DijkstraNode*>& GetNodes();
   void DeepCalculateMetrics(const std::string& _a, bool _isFirstCall = true);
-  std::stack<Container> AssembleRoute(const std::string& _a,
-                                      const std::string& _b);
+  std::queue<DijkstraBranch*> AssembleRoute(const std::string& _a,
+                                            const std::string& _b);
   void ResetDijkstraCalculationData();
-  void PlotRoute(const std::string& _a,
-                 const std::string& _b,
-                 std::function<void(std::stack<Container>&)> _ExportRoute =
-                     [](std::stack<Container>&) {});
+  std::queue<DijkstraBranch*> PlotRoute(const std::string& _a,
+                                        const std::string& _b);
 };
 }  // namespace nav
 #endif  // INCLUDES_NAVIGATION_HPP_
--- sources/Navigation.cpp
+++ sources/Navigation.cpp
@@ -3,59 +3,65 @@
 #include "Navigation.hpp"
 #include <cmath>
 #include <exception>
-#include <iostream>
+#include <utility>
 
-nav::DijkstraGraph::DijkstraNode& nav::DijkstraGraph::FindNode(
+nav::DijkstraGraph::DijkstraNode* nav::DijkstraGraph::FindNode(
     const std::string& _id) {
-  for (DijkstraNode& _node : nodes) {
-    if (_node.container.id == _id) {
+  for (DijkstraNode* _node : nodes) {
+    if (_node->container->id == _id) {
       return _node;
     }
   }
   throw std::invalid_argument("Bad ID (Not present in database)");
 }
 
-void nav::DijkstraGraph::AddNode(Container& _container) {
-  nodes.push_back(DijkstraNode(_container));
+void nav::DijkstraGraph::AddNode(nav::MapNode* _container) {
+  nodes.push_back(new DijkstraNode(_container));
 }
 
-void nav::DijkstraGraph::AddBranch(const std::string& _first,
-                                   const std::string& _second,
+void nav::DijkstraGraph::AddBranch(const std::string& _firstId,
+                                   nav::BranchType _firstBranchType,
+                                   const std::string& _secondId,
+                                   nav::BranchType _secondBranchType,
                                    double _weight) {
-  FindNode(_first).neighbors.push_back(std::make_pair(_second, _weight));
-  FindNode(_second).neighbors.push_back(std::make_pair(_first, _weight));
+  auto _nodeFirst = FindNode(_firstId);
+  auto _nodeSecond = FindNode(_secondId);
+  _nodeFirst->branches.push_back(
+      new DijkstraBranch(_nodeSecond, _weight, _firstBranchType));
+  _nodeSecond->branches.push_back(
+      new DijkstraBranch(_nodeFirst, _weight, _secondBranchType));
 }
 
-const std::vector<nav::DijkstraGraph::DijkstraNode>&
+const std::vector<nav::DijkstraGraph::DijkstraNode*>&
 nav::DijkstraGraph::GetNodes() {
   return nodes;
 }
 
 void nav::DijkstraGraph::DeepCalculateMetrics(const std::string& _a,
                                               bool _isFirstCall) {
-  DijkstraNode& _anchor = FindNode(_a);
+  DijkstraNode* _anchor = FindNode(_a);
   if (_isFirstCall) {
-    _anchor.nodeWeight = 0;
+    _anchor->nodeWeight = 0;
   }
-  _anchor.isVisited = true;
-  for (const std::pair<std::string, double>& _neighbor : _anchor.neighbors) {
-    DijkstraNode& _current = FindNode(_neighbor.first);
-    if (!_current.isVisited) {
-      if (_current.nodeWeight > _anchor.nodeWeight + _neighbor.second) {
-        _current.nodeWeight = _anchor.nodeWeight + _neighbor.second;
+  _anchor->isVisited = true;
+  for (DijkstraBranch* _branch : _anchor->branches) {
+    DijkstraNode* _current = _branch->node;
+    if (!_current->isVisited) {
+      if (_current->nodeWeight > _anchor->nodeWeight + _branch->weight) {
+        _current->nodeWeight = _anchor->nodeWeight + _branch->weight;
       }
     }
   }
-  std::string _minWeightNodeID = _anchor.container.id;
+  std::string _minWeightNodeID = _anchor->container->id;
   double _min = 0;
-  for (const DijkstraNode& _node : nodes) {
-    if (!_node.isVisited &&
-        (_node.nodeWeight < _min || _minWeightNodeID == _anchor.container.id)) {
-      _min = _node.nodeWeight;
-      _minWeightNodeID = _node.container.id;
+  for (const DijkstraNode* _node : nodes) {
+    if (!_node->isVisited &&
+        (_node->nodeWeight < _min || _minWeightNodeID == _anchor->container->id)) {
+      _min = _node->nodeWeight;
+      _minWeightNodeID = _node->container->id;
     }
   }
-  if (_minWeightNodeID != _anchor.container.id) {
+  if (_minWeightNodeID != _anchor->container->id) {
     DeepCalculateMetrics(_minWeightNodeID, false);
   }
 }
@@ -64,40 +70,50 @@ bool DoubleEpsilonCompare(double _a, double _b) {
   return abs((_a - _b)) < std::numeric_limits<double>::epsilon();
 }
 
-std::stack<nav::Container> nav::DijkstraGraph::AssembleRoute(
-    const std::string& _a,
-    const std::string& _b) {
-  std::stack<Container> _route;
-  DijkstraNode _current = FindNode(_b);
-  _route.push(_current.container);
-  double _routeRemainingLength = _current.nodeWeight;
+std::queue<nav::DijkstraGraph::DijkstraBranch*>
+nav::DijkstraGraph::AssembleRoute(const std::string& _a,
+                                  const std::string& _b) {
+  std::stack<DijkstraBranch*> _route;
+  DijkstraNode* _current = FindNode(_b);
+  _route.push(new DijkstraBranch(_current, 0.0, BranchType::Destination));
+  double _routeRemainingLength = _current->nodeWeight;
   while (!DoubleEpsilonCompare(_routeRemainingLength, 0.0)) {
-    for (const std::pair<std::string, double>& _neighbor : _current.neighbors) {
-      if (DoubleEpsilonCompare((_routeRemainingLength - _neighbor.second),
-                               FindNode(_neighbor.first).nodeWeight)) {
-        _routeRemainingLength -= _neighbor.second;
-        _current = FindNode(_neighbor.first);
-        _route.push(_current.container);
+    for (DijkstraBranch* _branch : _current->branches) {
+      if (DoubleEpsilonCompare((_routeRemainingLength - _branch->weight),
+                               FindNode(_branch->node->container->id)->nodeWeight)) {
+        _routeRemainingLength -= _branch->weight;
+        _route.push(_branch);
+        _current = FindNode(_branch->node->container->id);
         break;
       }
     }
   }
-  return _route;
+  std::queue<DijkstraBranch*> _parsedRoute;
+  double _lastBranchWeight = 0.0;
+  while (!_route.empty()) {
+    DijkstraBranch* _buffer = _route.top();
+    _route.pop();
+    std::swap(_buffer->weight, _lastBranchWeight);
+    if (_buffer->node->container->id == _a) {
+      _buffer->branchType = BranchType::StartingPoint;
+    }
+    _parsedRoute.push(_buffer);
+  }
+  return _parsedRoute;
 }
 
 void nav::DijkstraGraph::ResetDijkstraCalculationData() {
-  for (DijkstraNode& _node : nodes) {
-    _node.isVisited = false;
-    _node.nodeWeight = std::numeric_limits<double>::infinity();
+  for (DijkstraNode* _node : nodes) {
+    _node->isVisited = false;
+    _node->nodeWeight = std::numeric_limits<double>::infinity();
   }
 }
 
-void nav::DijkstraGraph::PlotRoute(
+std::queue<nav::DijkstraGraph::DijkstraBranch*> nav::DijkstraGraph::PlotRoute(
     const std::string& _a,
-    const std::string& _b,
-    std::function<void(std::stack<Container>&)> _ExportRoute) {
+    const std::string& _b) {
   DeepCalculateMetrics(_a);
   auto _route = AssembleRoute(_a, _b);
   ResetDijkstraCalculationData();
-  _ExportRoute(_route);
+  return _route;
 }
